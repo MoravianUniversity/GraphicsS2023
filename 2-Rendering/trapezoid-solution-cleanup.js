65ba8ad4c167604dde09ea257426f17433b9fa7f
Jeffrey Bush <jeff@coderforlife.com>
2023-01-27 10:36:02 -0500

    Adding trapezoid solutions.

---
+++ 2-Rendering/trapezoid-solution-cleanup.js
@@ -0,0 +1,111 @@
+// Renders a trapezoid (solution with the code cleaned up a bit)
+'use strict';
+    
+// Global WebGL context variable
+let gl;
+
+
+// Once the document is fully loaded run this init function.
+window.addEventListener('load', function init() {
+    // Get the HTML5 canvas object from it's ID
+    const canvas = document.getElementById('webgl-canvas');
+    if (!canvas) { window.alert('Could not find #webgl-canvas'); return; }
+
+    // Get the WebGL context (save into a global variable)
+    gl = canvas.getContext('webgl2');
+    if (!gl) { window.alert("WebGL isn't available"); return; }
+
+    // Configure WebGL
+    gl.viewport(0, 0, canvas.width, canvas.height); // this is the region of the canvas we want to draw on (all of it)
+    gl.clearColor(0.0, 0.0, 1.0, 1.0); // setup the background color with red, green, blue, and alpha amounts (this is white)
+    
+    // Initialize the WebGL program and data
+    gl.program = initProgram();
+    initBuffers();
+
+    // Render the static scene
+    render();
+});
+
+
+/**
+ * Initializes the WebGL program.
+ */
+function initProgram() {
+    // Compile shaders
+    // Vertex Shader: simplest possible
+    let vert_shader = compileShader(gl, gl.VERTEX_SHADER,
+        `#version 300 es
+        precision mediump float;
+
+        in vec4 aPosition;
+        
+        void main() {
+            gl_Position = aPosition;
+        }`
+    );
+    // Fragment Shader: simplest possible, chosen color is red for each point
+    let frag_shader = compileShader(gl, gl.FRAGMENT_SHADER,
+        `#version 300 es
+        precision mediump float;
+
+        out vec4 fragColor;
+
+        void main() {
+            fragColor = vec4(1.0, 0.0, 0.0, 1.0);
+        }`
+    );
+
+    // Link the shaders into a program and use them with the WebGL context
+    let program = linkProgram(gl, vert_shader, frag_shader);
+    gl.useProgram(program);
+    
+    // Get the position attribute index
+    program.aPosition = gl.getAttribLocation(program, 'aPosition'); // get the vertex shader attribute "aPosition"
+    
+    return program;
+}
+
+
+/**
+ * Initialize the data buffers.
+ */
+function initBuffers() {
+    // The vertices of the trapezoid
+    let coords = [-0.9, -0.9, -0.5, 0.9, 0, -0.9, 0.5, 0.9, 0.9, -0.9];
+    let indices = [0, 2, 1, 1, 2, 3, 2, 4, 3];
+
+    // Load the vertex coordinate data onto the GPU
+    gl.posBuffer = gl.createBuffer(); // create a new buffer
+    gl.bindBuffer(gl.ARRAY_BUFFER, gl.posBuffer); // bind to the new buffer
+    gl.bufferData(gl.ARRAY_BUFFER, Float32Array.from(coords), gl.STATIC_DRAW); // load the data into the buffer
+
+    // Load the index data onto the GPU
+    gl.indBuffer = gl.createBuffer(); // create a new buffer
+    gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, gl.indBuffer); // bind to the new buffer
+    gl.bufferData(gl.ELEMENT_ARRAY_BUFFER, Uint16Array.from(indices), gl.STATIC_DRAW); // load the data into the buffer
+    
+    // Cleanup
+    gl.bindBuffer(gl.ARRAY_BUFFER, null);
+    gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, null);
+}
+
+
+/**
+ * Render the scene.
+ */
+function render() {
+    // Clear the current rendering
+    gl.clear(gl.COLOR_BUFFER_BIT);
+    
+    // Associate the position attribute with the position buffer
+    gl.bindBuffer(gl.ARRAY_BUFFER, gl.posBuffer); // bind to the position buffer
+    gl.vertexAttribPointer(gl.program.aPosition, 2, gl.FLOAT, false, 0, 0); // associate the buffer with "aPosition" as length-2 vectors of floats
+    gl.enableVertexAttribArray(gl.program.aPosition); // enable this set of data
+    gl.bindBuffer(gl.ARRAY_BUFFER, null); // cleanup
+
+    // Draw the trapezoid
+    gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, gl.indBuffer); // bind the element buffer to draw
+    gl.drawElements(gl.TRIANGLES, 9, gl.UNSIGNED_SHORT, 0);
+    gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, null); // cleanup
+}
