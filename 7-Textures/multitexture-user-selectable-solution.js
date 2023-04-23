5c13b2249a603e1f15d2be7e7c4e57e0a1bcc86e
Jeffrey Bush <jeff@coderforlife.com>
2023-04-23 17:18:39 -0400

    Adding new texture examples and some solutions

---
+++ 7-Textures/multitexture-user-selectable-solution.js
@@ -0,0 +1,422 @@
+// A basic demo of using multiple textures that are user selectable (solution)
+'use strict';
+
+// Allow use of glMatrix values directly instead of needing the glMatrix prefix
+const vec3 = glMatrix.vec3;
+const vec4 = glMatrix.vec4;
+const mat4 = glMatrix.mat4;
+const quat = glMatrix.quat;
+
+// Global WebGL context variable
+let gl;
+
+// View values
+let position = [0, 0, -5];
+let rotation = [0, 0, 0];
+let scale = [1, 1, 1];
+
+// Objects to be drawn
+let obj;
+
+
+// Once the document is fully loaded run this init function.
+window.addEventListener('load', function init() {
+    // Get the HTML5 canvas object from it's ID
+    const canvas = document.getElementById('webgl-canvas');
+    if (!canvas) { window.alert('Could not find #webgl-canvas'); return; }
+
+    // Get the WebGL context (save into a global variable)
+    gl = canvas.getContext('webgl2');
+    if (!gl) { window.alert("WebGL isn't available"); return; }
+
+    // Configure WebGL
+    gl.viewport(0, 0, canvas.width, canvas.height); // this is the region of the canvas we want to draw on (all of it)
+    gl.clearColor(0, 0, 0, 0); // setup the background color with red, green, blue, and alpha
+    gl.enable(gl.DEPTH_TEST);
+    gl.enable(gl.CULL_FACE);
+    gl.cullFace(gl.BACK);
+
+    // Initialize the WebGL program and data
+    gl.program = initProgram();
+    initBuffers();
+    initTextures();
+    initEvents();
+
+    // Set initial values of uniforms
+    updateModelViewMatrix();
+    gl.uniform1i(gl.program.uTexture, +document.getElementById('texture').value);
+
+    // Render the static scene
+    onWindowResize();
+    //render(); // wait till images are loaded
+});
+
+
+/**
+ * Initializes the WebGL program.
+ */
+function initProgram() {
+    // Compile shaders
+    // Vertex Shader
+    let vert_shader = compileShader(gl, gl.VERTEX_SHADER,
+        `#version 300 es
+        precision mediump float;
+
+        // Matrices
+        uniform mat4 uModelViewMatrix;
+        uniform mat4 uProjectionMatrix;
+
+        // Light Position
+        const vec4 light = vec4(0, 0, 5, 1);
+
+        // Attributes for the vertex (from VBOs)
+        in vec4 aPosition;
+        in vec3 aNormal;
+        in vec2 aTexCoord;
+
+        // Vectors (varying variables to vertex shader)
+        out vec3 vNormalVector;
+        out vec3 vLightVector;
+        out vec3 vEyeVector;
+
+        // Texture information
+        out vec2 vTexCoord;
+
+        void main() {
+            vec4 P = uModelViewMatrix * aPosition;
+
+            vNormalVector = mat3(uModelViewMatrix) * aNormal;
+            vLightVector = light.w == 1.0 ? P.xyz - light.xyz : light.xyz;
+            vEyeVector = -P.xyz;
+
+            gl_Position = uProjectionMatrix * P;
+
+            vTexCoord = aTexCoord;
+        }`
+    );
+    // Fragment Shader - Phong Shading and Reflections
+    let frag_shader = compileShader(gl, gl.FRAGMENT_SHADER,
+        `#version 300 es
+        precision mediump float;
+
+        // Light and material properties
+        const vec3 lightColor = vec3(1, 1, 1);
+        const vec4 materialColor = vec4(0, 1, 0, 1);
+        const float materialAmbient = 0.2;
+        const float materialDiffuse = 0.5;
+        const float materialSpecular = 0.3;
+        const float materialShininess = 10.0;
+
+        // Vectors (varying variables from vertex shader)
+        in vec3 vNormalVector;
+        in vec3 vLightVector;
+        in vec3 vEyeVector;
+
+        // Texture information
+        uniform sampler2D uTexture;
+        in vec2 vTexCoord;
+
+        // Output color
+        out vec4 fragColor;
+
+        void main() {
+            // Normalize vectors
+            vec3 N = normalize(vNormalVector);
+            vec3 L = normalize(vLightVector);
+            vec3 E = normalize(vEyeVector);
+
+            // Compute lighting
+            float diffuse = dot(-L, N);
+            float specular = 0.0;
+            if (diffuse < 0.0) {
+                diffuse = 0.0;
+            } else {
+                vec3 R = reflect(L, N);
+                specular = pow(max(dot(R, E), 0.0), materialShininess);
+            }
+            
+            // TODO: Object color combined from texture and material
+            vec4 color = texture(uTexture, vTexCoord);
+
+            // Compute final color
+            fragColor.rgb = lightColor * (
+                (materialAmbient + materialDiffuse * diffuse) * color.rgb +
+                materialSpecular * specular);
+            fragColor.a = 1.0;
+        }`
+    );
+
+    // Link the shaders into a program and use them with the WebGL context
+    let program = linkProgram(gl, vert_shader, frag_shader);
+    gl.useProgram(program);
+    
+    // Get the attribute indices
+    program.aPosition = gl.getAttribLocation(program, 'aPosition');
+    program.aNormal = gl.getAttribLocation(program, 'aNormal');
+    program.aTexCoord = gl.getAttribLocation(program, 'aTexCoord');
+
+    // Get the uniform indices
+    program.uModelViewMatrix = gl.getUniformLocation(program, 'uModelViewMatrix');
+    program.uProjectionMatrix = gl.getUniformLocation(program, 'uProjectionMatrix');
+    program.uTexture = gl.getUniformLocation(program, 'uTexture');
+
+    return program;
+}
+
+
+/**
+ * Initialize the data buffers.
+ */
+function initBuffers() {
+    // The vertices, colors, and indices for a cube
+    let cube_coords = [
+        1, 1, 1,    // A
+        -1, 1, 1,   // B
+        -1, -1, 1,  // C
+        1, -1, 1,   // D
+        1, -1, -1,  // E
+        -1, -1, -1, // F
+        -1, 1, -1,  // G
+        1, 1, -1,   // H
+    ];
+    let cube_tex_coords = [
+        1, 1, // A
+        0, 1, // B
+        0, 0, // C
+        1, 0, // D
+        1, 1, // E
+        0, 1, // F
+        0, 0, // G
+        1, 0, // H
+    ];
+    let cube_indices = [
+        1, 2, 0, 2, 3, 0,
+        7, 6, 1, 0, 7, 1,
+        1, 6, 2, 6, 5, 2,
+        3, 2, 4, 2, 5, 4,
+        6, 7, 5, 7, 4, 5,
+        0, 3, 7, 3, 4, 7,
+    ];
+    obj = createObject(cube_coords, cube_tex_coords, cube_indices, false);
+}
+
+
+/**
+ * Creates a VAO containing the coordinates, colors, and indices provided
+ */
+function createObject(coords, tex_coords, indices, is_tri_strip) {
+    coords = Float32Array.from(coords);
+    tex_coords = Float32Array.from(tex_coords);
+    let normals = coords;
+
+    // Create and bind VAO
+    let vao = gl.createVertexArray();
+    gl.bindVertexArray(vao);
+
+    // Load the coordinate data into the GPU and associate with shader
+    let buf = gl.createBuffer();
+    gl.bindBuffer(gl.ARRAY_BUFFER, buf);
+    gl.bufferData(gl.ARRAY_BUFFER, coords, gl.STATIC_DRAW);
+    gl.vertexAttribPointer(gl.program.aPosition, 3, gl.FLOAT, false, 0, 0);
+    gl.enableVertexAttribArray(gl.program.aPosition);
+
+    // Load the normal data into the GPU and associate with shader
+    buf = gl.createBuffer();
+    gl.bindBuffer(gl.ARRAY_BUFFER, buf);
+    gl.bufferData(gl.ARRAY_BUFFER, normals, gl.STATIC_DRAW);
+    gl.vertexAttribPointer(gl.program.aNormal, 3, gl.FLOAT, false, 0, 0);
+    gl.enableVertexAttribArray(gl.program.aNormal);
+    
+    // Load the texture coordinate data into the GPU and associate with shader
+    buf = gl.createBuffer();
+    gl.bindBuffer(gl.ARRAY_BUFFER, buf);
+    gl.bufferData(gl.ARRAY_BUFFER, tex_coords, gl.STATIC_DRAW);
+    gl.vertexAttribPointer(gl.program.aTexCoord, 2, gl.FLOAT, false, 0, 0);
+    gl.enableVertexAttribArray(gl.program.aTexCoord);
+
+    // Load the index data into the GPU
+    buf = gl.createBuffer();
+    gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, buf);
+    gl.bufferData(gl.ELEMENT_ARRAY_BUFFER, Uint16Array.from(indices), gl.STATIC_DRAW);
+
+    // Cleanup
+    gl.bindVertexArray(null);
+    gl.bindBuffer(gl.ARRAY_BUFFER, null);
+    gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, null);
+
+    // Return the object information
+    return [vao, indices.length, is_tri_strip ? gl.TRIANGLE_STRIP : gl.TRIANGLES];
+}
+
+
+/**
+ * Load a texture onto the GPU. The second argument is the texture number, defaulting to 0.
+ */
+function loadTexture(img, index) {
+    // Default argument value
+    if (typeof index === 'undefined') { index = 0; }
+
+    let texture = gl.createTexture(); // create a texture resource on the GPU
+    gl.activeTexture(gl['TEXTURE' + index]); // set the current texture that all following commands will apply to
+    gl.bindTexture(gl.TEXTURE_2D, texture); // assign our texture resource as the current texture
+    gl.pixelStorei(gl.UNPACK_FLIP_Y_WEBGL, true); // tell WebGL to flip the image vertically (almost always want this to be true)
+
+    // Load the image data into the texture
+    gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGBA, gl.RGBA, gl.UNSIGNED_BYTE, img);
+
+    // Setup options for downsampling and upsampling the image data
+    gl.generateMipmap(gl.TEXTURE_2D);
+    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.NEAREST_MIPMAP_LINEAR);
+    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.LINEAR);
+
+    // Cleanup and return
+    gl.bindTexture(gl.TEXTURE_2D, null);
+    return texture;
+}
+
+
+/**
+ * Initialize the texture buffers.
+ */
+function initTextures() {
+    obj.push(loadTexture(createCheckerboardImage(128, 2), 0));
+
+    let image = new Image();
+    image.src = 'WebGL.png';
+    image.addEventListener('load', () => {
+        obj.push(loadTexture(image, 1));
+        render();
+    });
+}
+
+
+/**
+ * Initialize event handlers
+ */
+function initEvents() {
+    window.addEventListener('resize', onWindowResize);
+    gl.canvas.addEventListener('mousedown', onMouseDown);
+    gl.canvas.addEventListener('wheel', onMouseWheel);
+    document.getElementById('texture').addEventListener('input', () => {
+        gl.uniform1i(gl.program.uTexture, +document.getElementById('texture').value);
+    });
+}
+
+
+/**
+ * Update the model view matrix.
+ */
+function updateModelViewMatrix() {
+    let mv = glMatrix.mat4.fromRotationTranslationScale(glMatrix.mat4.create(),
+        glMatrix.quat.fromEuler(glMatrix.quat.create(), ...rotation), position, scale);
+    gl.uniformMatrix4fv(gl.program.uModelViewMatrix, false, mv);
+}
+
+
+/**
+ * Handle the click-and-drag to rotate the cube.
+ */
+function onMouseDown(e) {
+    e.preventDefault();
+
+    let [startX, startY] = [e.offsetX, e.offsetY];
+    let start_rotation = rotation.slice();
+    function onMouseMove(e2) {
+        let x_rotation = (e2.offsetX - startX) / (this.width - 1) * 360;
+        let y_rotation = (e2.offsetY - startY) / (this.height - 1) * 360;
+        rotation[0] = start_rotation[0] + y_rotation;
+        rotation[1] = start_rotation[1] + x_rotation;
+        updateModelViewMatrix();
+    }
+    function onMouseUp() {
+        this.removeEventListener('mousemove', onMouseMove);
+        this.removeEventListener('mouseup', onMouseUp);
+    }
+    if (e.button === 0) {
+        this.addEventListener('mousemove', onMouseMove);
+        this.addEventListener('mouseup', onMouseUp);
+    }
+}
+
+
+/**
+ * "Zoom" when using the mouse wheel.
+ */
+function onMouseWheel(e) {
+    let s = scale[0] * Math.pow(1.05, e.deltaY);
+    scale = [s, s, s];
+    updateModelViewMatrix();
+}
+
+
+/**
+ * Update the projection matrix.
+ */
+function updateProjectionMatrix() {
+    let aspect = gl.canvas.width / gl.canvas.height;
+    let p = mat4.perspective(mat4.create(), Math.PI / 4, aspect, 0.1, 10);
+    gl.uniformMatrix4fv(gl.program.uProjectionMatrix, false, p);
+}
+
+
+/**
+ * Keep the canvas sized to the window.
+ */
+function onWindowResize() {
+    gl.canvas.width = window.innerWidth;
+    gl.canvas.height = window.innerHeight;
+    gl.viewport(0, 0, gl.canvas.width, gl.canvas.height);
+    updateProjectionMatrix();
+}
+
+
+/**
+ * Render the scene. Must be called once and only once. It will call itself again.
+ */
+function render() {
+    gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
+
+    let [vao, count, mode, texture1, texture2] = obj;
+
+    // Activate and bind both textures
+    gl.activeTexture(gl.TEXTURE0);
+    gl.bindTexture(gl.TEXTURE_2D, texture1);
+    gl.activeTexture(gl.TEXTURE1);
+    gl.bindTexture(gl.TEXTURE_2D, texture2);
+
+    // Draw
+    gl.bindVertexArray(vao);
+    gl.drawElements(mode, count, gl.UNSIGNED_SHORT, 0);
+
+    // Cleanup
+    gl.bindVertexArray(null);
+    gl.activeTexture(gl.TEXTURE0);
+    gl.bindTexture(gl.TEXTURE_2D, null);
+    gl.activeTexture(gl.TEXTURE1);
+    gl.bindTexture(gl.TEXTURE_2D, null);
+
+    // Render again
+    window.requestAnimationFrame(render);
+}
+
+
+/**
+ * Create a checkerboard image of white and black squares. The image will be size-by-size pixels.
+ * There will be a total of num_checks boxes in each direction for a total of num_checks^2.
+ */
+function createCheckerboardImage(size, num_checks) {
+    let img = new ImageData(size, size);
+    let data = img.data;
+    let checkSize = size/num_checks;
+    for (let i = 0; i < size; i++) {
+        for (let j = 0; j < size; j++) {
+            let off = 4*(i*size+j);
+            let checkX = Math.floor(i/checkSize)%2;
+            let checkY = Math.floor(j/checkSize)%2;
+            let c = (checkX !== checkY) ? 255 : 0;
+            data[off] = data[off+1] = data[off+2] = c;
+            data[off+3] = 255;
+        }
+    }
+    return img;
+}
